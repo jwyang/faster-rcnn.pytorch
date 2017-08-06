1a5f61a0693910ae5aacfdcfcb00e7c680aa0dfe
jiasen <jiasenlu@vt.edu>
2017-08-06 15:32:56 -0400

    init

---
+++ tools/trainval_net.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------
+# Tensorflow Faster R-CNN
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Jiasen Lu, Jianwei Yang, based on code from Ross Girshick
+# --------------------------------------------------------
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+
+
