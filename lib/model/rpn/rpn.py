09f9cf61192e5a7af2dffab34208aa1bfb591a83
Jianwei Yang <jyang375@vicki.cc.gatech.edu>
2017-08-06 17:47:22 -0400

    nms done, rpn ongoing

--- lib/model/rpn/rpn.py
+++ lib/model/rpn/rpn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+from proposal_layer import _ProposalLayer
 
 class _RPN(nn.Module):
     """ region proposal network """
