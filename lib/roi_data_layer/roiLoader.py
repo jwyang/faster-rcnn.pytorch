288f7416da7b98268bb7c9085a28bea51984d623
Jianwei Yang <jyang375@vicki.cc.gatech.edu>
2017-08-14 01:30:12 -0400

    restore roiLoader.py

--- lib/roi_data_layer/roiLoader.py
+++ lib/roi_data_layer/roiLoader.py
@@ -31,36 +31,6 @@ class roiLoader(data.Dataset):
     data = torch.from_numpy(blobs['data'])
     im_info = torch.from_numpy(blobs['im_info'])
     gt_boxes = torch.from_numpy(blobs['gt_boxes'])
-
-    ################################################
-    # we trim the image according to the gt_boxes, #
-    # to retrain the gt boxes as much as possible  #
-    ################################################
-
-    x_left_most,_   = torch.min(gt_boxes[:, 0], 0)
-    y_top_most, _   = torch.min(gt_boxes[:, 1], 0)
-    x_right_most,_  = torch.max(gt_boxes[:, 2], 0)
-    y_bottom_most,_ = torch.max(gt_boxes[:, 3], 0)
-
-    trim_data = torch.FloatTensor(1, self.trim_height, self.trim_width, 3)
-    u_height = np.min((self.trim_height, data.size(1)))
-    u_width = np.min((self.trim_width, data.size(2)))
-
-    # copy data to trim data
-    trim_data[1, :u_height, :u_width, :] = data[1, :u_height, :u_width, :]
-
-    # we trim image a bit to adapt to the trim_height
-    if data.size(1) > self.trim_height:
-        trim_data[0, :, :data.size(2)]
-    # we trim image a bit to adapt to the trim_width
-    if data.size(2) > self.trim_width:
-
-    # if exceeds the image width
-    if (x_right_most - x_left_most) > self.trim_width:
-        print("trim")
-
-    pdb.set_trace()
-
     return data, im_info, gt_boxes
 
   def __len__(self):
