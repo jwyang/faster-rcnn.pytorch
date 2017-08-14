1cd3d6a31bf03bb97d059670f1b11eb1ccb8693e
jwyang <jw2.yang@gmail.com>
2017-08-14 10:54:29 -0400

    finished and checked roi batch loader

--- lib/roi_data_layer/roibatchLoader.py
+++ lib/roi_data_layer/roibatchLoader.py
@@ -18,13 +18,13 @@ import numpy as np
 import time
 import pdb
 
-class roiLoader(data.Dataset):
-  def __init__(self, roidb, num_classes, imgsize):
+class roibatchLoader(data.Dataset):
+  def __init__(self, roidb, num_classes):
     self._roidb = roidb
     self._num_classes = num_classes
     # we make the height of image consistent to trim_height, trim_width
-    self.trim_height = imgsize
-    self.trim_width = imgsize
+    self.trim_height = 600 # cfg.TRAIN.TRIM_HEIGHT
+    self.trim_width = 600 # cfg.TRAIN.TRIM_WIDTH
 
   def __getitem__(self, index):
 
@@ -39,43 +39,50 @@ class roiLoader(data.Dataset):
     ##################################################
 
     data_height, data_width = data.size(1), data.size(2)
-    trim_data = torch.FloatTensor(1, self.trim_height, self.trim_width, 3)
-
-    x_left_most,_   = torch.min(gt_boxes[:, 0], 0)
-    y_top_most, _   = torch.min(gt_boxes[:, 1], 0)
-    x_right_most,_  = torch.max(gt_boxes[:, 2], 0)
-    y_bottom_most,_ = torch.max(gt_boxes[:, 3], 0)
+    # trim_data = torch.FloatTensor(1, self.trim_height, self.trim_width, 3)
 
     if data_height > data_width:
         # if height > width, then crop on height
         # randomly generate an y start point
-        y_s = np.random.randint(data_height - self.trim_height)
-        trim_data = data[:, y_s:(y_s + self.trim_height), :, :]
+        y_s = np.random.randint(data_height - self.trim_height + 1)
+        trim_data = data[:, y_s:(y_s + self.trim_height), :]
 
-        # find the bound box outside the image crop
-        keep = gt_boxes[:, 3] > y_s and \
-               gt_boxes[:, 1] < (y_s + self.trim_height)
-        gt_boxes = gt_boxes[keep, :]
+        # shift y coordiante of gt_boxes
+        gt_boxes[:, 1] = gt_boxes[:, 1] - y_s
+        gt_boxes[:, 3] = gt_boxes[:, 3] - y_s        
 
         # update gt bounding box according the trip
-        gt_boxes = clip_boxes(gt_boxes, [self.trim_height, self.trim_width])
+        gt_boxes[:, 1].clamp(0, self.trim_height)
+        gt_boxes[:, 3].clamp(0, self.trim_height)
+
+        # update im_info
+        im_info[0, 0] = self.trim_height
 
     elif data_height <= data_width:
         # if height <= width, then crop on width
-        x_s = np.random.randint(data_width - self.trim_width)
-        trim_data = data[:, :, x_s:(x_s + self.trim_width), :, :]
-        # find the bound box outside the image crop
-        keep = gt_boxes[:, 2] > y_s and \
-               gt_boxes[:, 0] < (y_s + self.trim_width)
-        gt_boxes = gt_boxes[keep, :]
+        x_s = np.random.randint(data_width - self.trim_width + 1)
+        trim_data = data[:, :, x_s:(x_s + self.trim_width), :]
+
+        # shift x coordiante of gt_boxes
+        gt_boxes[:, 0] = gt_boxes[:, 0] - x_s
+        gt_boxes[:, 2] = gt_boxes[:, 2] - x_s
 
         # update gt bounding box according the trip
-        gt_boxes = clip_boxes(gt_boxes, [self.trim_height, self.trim_width])
+        gt_boxes[:, 0].clamp(0, self.trim_width)
+        gt_boxes[:, 2].clamp(0, self.trim_width)
 
+        # update im_info
+        im_info[0, 1] = self.trim_width
+
+    # append img index to im_info and gt_boxes
+    ind = torch.FloatTensor(gt_boxes.size(0), 1).fill_(index)
+    im_info = torch.cat((im_info, ind[0]), 1)
+    gt_boxes = torch.cat((gt_boxes, ind), 1)
+
+    # permute trim_data to adapt to downstream processing
     trim_data.permute(0, 3, 1, 2)
-    pdb.set_trace()
 
-    return data, im_info, gt_boxes
+    return trim_data, im_info, gt_boxes
 
   def __len__(self):
     return len(self._roidb)
