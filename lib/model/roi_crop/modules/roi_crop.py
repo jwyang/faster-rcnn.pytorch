7929d68e8d79bae487858fef93e81d28aa708d40
Jianwei Yang <jyang375@t1000.cc.gatech.edu>
2017-12-21 02:26:27 -0500

    update roi_crop, fix a minor bug for multii-gpu

--- lib/model/roi_crop/modules/roi_crop.py
+++ lib/model/roi_crop/modules/roi_crop.py
@@ -4,6 +4,5 @@ from ..functions.roi_crop import RoICropFunction
 class _RoICrop(Module):
     def __init__(self, layout = 'BHWD'):
         super(_RoICrop, self).__init__()
-        self.f = RoICropFunction()
     def forward(self, input1, input2):
-        return self.f(input1, input2)
+        return RoICropFunction()(input1, input2)
