4ab16030a411702396b1e10dcb30b84ec0893c07
Jianwei Yang <jyang375@t1000.cc.gatech.edu>
2017-12-27 12:25:31 -0500

    minor change to nms_wrapper

--- lib/model/nms/nms_wrapper.py
+++ lib/model/nms/nms_wrapper.py
@@ -6,19 +6,13 @@
 # --------------------------------------------------------
 import torch
 from model.utils.config import cfg
-from model.nms.cpu_nms import cpu_nms
 from model.nms.nms_gpu import nms_gpu
 
 def nms(dets, thresh, force_cpu=False):
     """Dispatch to either CPU or GPU NMS implementations."""
     if dets.shape[0] == 0:
         return []
-    if cfg.USE_GPU_NMS and not force_cpu:
-        # ---numpy version---
-        # original: return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
-        # ---pytorch version---
-        return nms_gpu(dets, thresh)
-    else:
-        keep = cpu_nms(dets.numpy(), thresh)
-
-        return torch.Tensor(keep)
+    # ---numpy version---
+    # original: return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
+    # ---pytorch version---
+    return nms_gpu(dets, thresh)
