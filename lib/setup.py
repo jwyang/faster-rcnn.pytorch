2675672f7de786d6e9b07f1f20564dd0ee35cb60
Jianwei Yang <jyang375@jarvis.cc.gatech.edu>
2017-12-28 11:39:24 -0500

    comment unused lines in setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -23,40 +23,40 @@ def find_in_path(name, path):
     return None
 
 
-def locate_cuda():
-    """Locate the CUDA environment on the system
+# def locate_cuda():
+#     """Locate the CUDA environment on the system
+#
+#     Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
+#     and values giving the absolute path to each directory.
+#
+#     Starts by looking for the CUDAHOME env variable. If not found, everything
+#     is based on finding 'nvcc' in the PATH.
+#     """
+# 
+#     # first check if the CUDAHOME env variable is in use
+#     if 'CUDAHOME' in os.environ:
+#         home = os.environ['CUDAHOME']
+#         nvcc = pjoin(home, 'bin', 'nvcc')
+#     else:
+#         # otherwise, search the PATH for NVCC
+#         default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
+#         nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
+#         if nvcc is None:
+#             raise EnvironmentError('The nvcc binary could not be '
+#                                    'located in your $PATH. Either add it to your path, or set $CUDAHOME')
+#         home = os.path.dirname(os.path.dirname(nvcc))
+#
+#     cudaconfig = {'home': home, 'nvcc': nvcc,
+#                   'include': pjoin(home, 'include'),
+#                   'lib64': pjoin(home, 'lib64')}
+#     for k, v in cudaconfig.iteritems():
+#         if not os.path.exists(v):
+#             raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+#
+#     return cudaconfig
 
-    Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
-    and values giving the absolute path to each directory.
 
-    Starts by looking for the CUDAHOME env variable. If not found, everything
-    is based on finding 'nvcc' in the PATH.
-    """
-
-    # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
-    else:
-        # otherwise, search the PATH for NVCC
-        default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
-        nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
-        if nvcc is None:
-            raise EnvironmentError('The nvcc binary could not be '
-                                   'located in your $PATH. Either add it to your path, or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
-
-    cudaconfig = {'home': home, 'nvcc': nvcc,
-                  'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.iteritems():
-        if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
-
-    return cudaconfig
-
-
-CUDA = locate_cuda()
+# CUDA = locate_cuda()
 
 # Obtain the numpy include directory.  This logic works across numpy versions.
 try:
@@ -112,11 +112,11 @@ class custom_build_ext(build_ext):
 
 
 ext_modules = [
-    Extension(		
-        "model.utils.cython_bbox",		
-        ["model/utils/bbox.pyx"],		
-        extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},		
-        include_dirs=[numpy_include]		
+    Extension(
+        "model.utils.cython_bbox",
+        ["model/utils/bbox.pyx"],
+        extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+        include_dirs=[numpy_include]
     ),
     Extension(
         'pycocotools._mask',
