db6ef7d0585d820c64e6daedaa5a86a2bbf27a79
Jianwei Yang <jyang375@bender.cc.gatech.edu>
2018-01-03 20:27:58 -0500

    add demo.py for testing

--- demo.py
+++ demo.py
@@ -24,7 +24,7 @@ import torch.optim as optim
 
 import torchvision.transforms as transforms
 import torchvision.datasets as dset
-from PIL import Image
+from scipy.misc import imread
 from roi_data_layer.roidb import combined_roidb
 from roi_data_layer.roibatchLoader import roibatchLoader
 from model.utils.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
@@ -232,10 +232,12 @@ if __name__ == '__main__':
       # Load the demo image
       im_file = os.path.join(args.image_dir, imglist[i])
       # im = cv2.imread(im_file)
-      im = np.array(Image.open(im_file))
-      if len(im.shape) == 2:
-        im = im[:,:,np.newaxis]
-        im = np.concatenate((im,im,im), axis=2)
+      im_in = np.array(imread(im_file))
+      if len(im_in.shape) == 2:
+        im_in = im_in[:,:,np.newaxis]
+        im_in = np.concatenate((im_in,im_in,im_in), axis=2)
+      # rgb -> bgr
+      im = im_in[:,:,::-1]
 
       blobs, im_scales = _get_image_blob(im)
       assert len(im_scales) == 1, "Only single-image batch implemented"
@@ -255,47 +257,57 @@ if __name__ == '__main__':
 
 
       det_tic = time.time()
-      rois, cls_prob, bbox_pred, rpn_loss, rcnn_loss = \
-          fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
 
+      rois, cls_prob, bbox_pred, rpn_loss, rcnn_loss = fasterRCNN(im_data, im_info, gt_boxes, num_boxes)
       scores = cls_prob.data
-      boxes = rois[:, :, 1:5] / im_scales[0]
+      boxes = rois.data[:, :, 1:5]
 
       if cfg.TEST.BBOX_REG:
           # Apply bounding-box regression deltas
           box_deltas = bbox_pred.data
           if cfg.TRAIN.BBOX_NORMALIZE_TARGETS_PRECOMPUTED:
           # Optionally normalize targets by a precomputed mean and stdev
+            if args.class_agnostic:
                 box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
                            + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
                 box_deltas = box_deltas.view(1, -1, 4)
+            else:
+                box_deltas = box_deltas.view(-1, 4) * torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_STDS).cuda() \
+                           + torch.FloatTensor(cfg.TRAIN.BBOX_NORMALIZE_MEANS).cuda()
+                box_deltas = box_deltas.view(1, -1, 4 * len(pascal_classes))
+
           pred_boxes = bbox_transform_inv(boxes, box_deltas, 1)
           pred_boxes = clip_boxes(pred_boxes, im_info.data, 1)
       else:
           # Simply repeat the boxes, once for each class
           pred_boxes = np.tile(boxes, (1, scores.shape[1]))
 
-      scores = scores.squeeze().cpu().numpy()
-      pred_boxes = pred_boxes.squeeze().cpu().numpy()
-      # _t['im_detect'].tic()
+      pred_boxes /= im_scales[0]
+
+      scores = scores.squeeze()    
+      pred_boxes = pred_boxes.squeeze()
       det_toc = time.time()
       detect_time = det_toc - det_tic
-
       misc_tic = time.time()
-
       if vis:
           im2show = np.copy(im)
+      for j in xrange(1, len(pascal_classes)):
+          inds = torch.nonzero(scores[:,j]>thresh).view(-1)
+          # if there is det
+          if inds.numel() > 0:
+            cls_scores = scores[:,j][inds]
+            _, order = torch.sort(cls_scores, 0, True)
+            if args.class_agnostic:
+              cls_boxes = pred_boxes[inds, :]
+            else:
+              cls_boxes = pred_boxes[inds][:, j * 4:(j + 1) * 4]
 
-      for j in xrange(1, 21):
-          inds = np.where(scores[:, j] > thresh)[0]
-          cls_scores = scores[inds, j]
-          cls_boxes = pred_boxes[inds, :]
-          cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
-              .astype(np.float32, copy=False)
-          keep = nms(cls_dets, cfg.TEST.NMS)
-          cls_dets = cls_dets[keep, :]
-          if vis:
-              im2show = vis_detections(im2show, classes[j], cls_dets)
+            cls_dets = torch.cat((cls_boxes, cls_scores), 1)
+            cls_dets = cls_dets[order]
+            keep = nms(cls_dets, cfg.TEST.NMS)
+            cls_dets = cls_dets[keep.view(-1).long()]
+            if vis:
+              im2show = vis_detections(im2show, pascal_classes[j], cls_dets.cpu().numpy(), 0.3)
 
       misc_toc = time.time()
       nms_time = misc_toc - misc_tic
