38c6b7b65dfbcbca56ff3fc5bebd7585f4da07d4
Jianwei Yang <jyang375@t1000.cc.gatech.edu>
2018-01-09 18:14:35 -0500

    adapt to resnet_ls.yml for large image size training

--- test_net.py
+++ test_net.py
@@ -57,6 +57,9 @@ def parse_args():
   parser.add_argument('--cuda', dest='cuda',
                       help='whether use CUDA',
                       action='store_true')
+  parser.add_argument('--ls', dest='large_scale',
+                      help='whether use large imag scale',
+                      action='store_true')                           
   parser.add_argument('--mGPUs', dest='mGPUs',
                       help='whether use multiple GPUs',
                       action='store_true')
@@ -120,7 +123,7 @@ if __name__ == '__main__':
       args.imdbval_name = "vg_150-50-50_minival"
       args.set_cfgs = ['ANCHOR_SCALES', '[4, 8, 16, 32]', 'ANCHOR_RATIOS', '[0.5,1,2]']
 
-  args.cfg_file = "cfgs/{}.yml".format(args.net)
+  args.cfg_file = "cfgs/{}_ls.yml".format(args.net) if args.large_scale else "cfgs/{}.yml".format(args.net)
 
   if args.cfg_file is not None:
     cfg_from_file(args.cfg_file)
