5f730a80c9f0d37650cb4c929e90f901a8cf5a77
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-14 11:37:27 +0800

    comment coco to disable the API

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -12,7 +12,7 @@ from __future__ import print_function
 
 __sets = {}
 from datasets.pascal_voc import pascal_voc
-from datasets.coco import coco
+# from datasets.coco import coco
 from datasets.imagenet import imagenet
 from datasets.vg import vg
 
