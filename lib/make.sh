448c8cca9c641f07aeeeb78b8e1911cc5e97c3cf
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-14 11:34:50 +0800

    set cuda mode to sm_52

--- lib/make.sh
+++ lib/make.sh
@@ -5,18 +5,11 @@ CUDA_PATH=/usr/local/cuda/
 python setup.py build_ext --inplace
 rm -rf build
 
-CUDA_ARCH="-gencode arch=compute_20,code=sm_20 \
-           -gencode arch=compute_30,code=sm_30 \
-           -gencode arch=compute_35,code=sm_35 \
-           -gencode arch=compute_50,code=sm_50 \
-           -gencode arch=compute_52,code=sm_52 \
-           -gencode arch=compute_52,code=sm_60 "
-
 # compile NMS
 cd model/nms/src
 echo "Compiling nms kernels by nvcc..."
 nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 
 cd ../
 python build.py
@@ -26,7 +19,7 @@ cd ../../
 cd model/roi_pooling/src
 echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../
 python build.py
 
@@ -35,7 +28,7 @@ cd ../../
 cd model/roi_align/src
 echo "Compiling roi align kernels by nvcc..."
 nvcc -c -o roi_align_kernel.cu.o roi_align_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../
 python build.py
 
@@ -44,6 +37,6 @@ cd ../../
 cd model/roi_crop/src
 echo "Compiling roi crop kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
 cd ../
 python build.py
