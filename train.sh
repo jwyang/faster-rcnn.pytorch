feb3a76c7e1165fbbc2a2c08f817d64090577932
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-14 17:49:45 +0800

    put logs to the `train`

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ len=${#array[@]}
 EXTRA_ARGS=${array[@]:3:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
-LOG="logs/${NET}_${DATASET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="logs/train/${NET}_${DATASET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
