e954277b629b36f198aefd4785311cb661efb74a
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-17 10:26:51 +0800

    reformat to fit PEP8

--- lib/model/roi_crop/modules/roi_crop.py
+++ lib/model/roi_crop/modules/roi_crop.py
@@ -1,8 +1,10 @@
 from torch.nn.modules.module import Module
 from ..functions.roi_crop import RoICropFunction
 
+
 class _RoICrop(Module):
-    def __init__(self, layout = 'BHWD'):
+    def __init__(self, layout='BHWD'):
         super(_RoICrop, self).__init__()
+
     def forward(self, input1, input2):
         return RoICropFunction()(input1, input2)
