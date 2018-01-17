2761041e034f384c9375108a4de676c743367b61
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-17 17:55:45 +0800

    Add files via upload

--- lib/model/nms/nms_gpu.py
+++ lib/model/nms/nms_gpu.py
@@ -3,9 +3,10 @@ import numpy as np
 from _ext import nms
 import pdb
 
+
 def nms_gpu(dets, thresh):
-	keep = dets.new(dets.size(0), 1).zero_().int()
-	num_out = dets.new(1).zero_().int()
-	nms.nms_cuda(keep, dets, num_out, thresh)
-	keep = keep[:num_out[0]]
-	return keep
+    keep = dets.new(dets.size(0), 1).zero_().int()
+    num_out = dets.new(1).zero_().int()
+    nms.nms_cuda(keep, dets, num_out, thresh)
+    keep = keep[:num_out[0]]
+    return keep
