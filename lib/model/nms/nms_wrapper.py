d23b165d9cf113ad67a348efa3ff80bef25a6d21
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-17 17:53:34 +0800

    reformat to fit PEP8

--- lib/model/nms/nms_wrapper.py
+++ lib/model/nms/nms_wrapper.py
@@ -8,6 +8,7 @@ import torch
 from model.utils.config import cfg
 from model.nms.nms_gpu import nms_gpu
 
+
 def nms(dets, thresh, force_cpu=False):
     """Dispatch to either CPU or GPU NMS implementations."""
     if dets.shape[0] == 0:
