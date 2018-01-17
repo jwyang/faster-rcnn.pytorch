cd138c76827590a6cc234ece0af57c6f7e7d6484
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-17 10:12:23 +0800

    use property `detection_dimension` instead of 4096

--- lib/model/faster_rcnn/vgg16.py
+++ lib/model/faster_rcnn/vgg16.py
@@ -16,47 +16,55 @@ import torchvision.models as models
 from model.faster_rcnn.faster_rcnn_cascade import _fasterRCNN
 import pdb
 
+
 class vgg16(_fasterRCNN):
-  def __init__(self, classes, pretrained=False, class_agnostic=False):
-    self.model_path = 'data/pretrained_model/vgg16_caffe.pth'
-    self.dout_base_model = 512
-    self.pretrained = pretrained
-    self.class_agnostic = class_agnostic
+    def __init__(self, classes, pretrained=False, class_agnostic=False):
+        self.model_path = 'data/pretrained_model/vgg16_caffe.pth'
+        self.dout_base_model = 512  # rpn channels
+        self.pretrained = pretrained
+        self.class_agnostic = class_agnostic
+        self.detection_dimension = 4096
 
-    _fasterRCNN.__init__(self, classes, class_agnostic)
+        _fasterRCNN.__init__(self, classes, class_agnostic)
 
-  def _init_modules(self):
-    vgg = models.vgg16()
-    if self.pretrained:
-        print("Loading pretrained weights from %s" %(self.model_path))
-        state_dict = torch.load(self.model_path)
-        vgg.load_state_dict({k:v for k,v in state_dict.items() if k in vgg.state_dict()})
+    def _init_modules(self):
+        vgg = models.vgg16()
+        if self.pretrained:
+            print("Loading pretrained weights from %s" % (self.model_path))
+            state_dict = torch.load(self.model_path)
+            vgg.load_state_dict(
+                {k: v for k, v in state_dict.items() if k in vgg.state_dict()})
 
-    vgg.classifier = nn.Sequential(*list(vgg.classifier._modules.values())[:-1])
+        vgg.classifier = nn.Sequential(
+            *list(vgg.classifier._modules.values())[:-1])
 
-    # not using the last maxpool layer
-    self.RCNN_base = nn.Sequential(*list(vgg.features._modules.values())[:-1])
+        # not using the last maxpool layer
+        self.RCNN_base = nn.Sequential(
+            *list(vgg.features._modules.values())[:-1])
 
-    # Fix the layers before conv3:
-    for layer in range(10):
-      for p in self.RCNN_base[layer].parameters(): p.requires_grad = False
+        # Fix the layers before conv3:
+        for layer in range(10):
+            for p in self.RCNN_base[layer].parameters():
+                p.requires_grad = False
 
-    # self.RCNN_base = _RCNN_base(vgg.features, self.classes, self.dout_base_model)
+        # self.RCNN_base = _RCNN_base(vgg.features, self.classes,
+        #                             self.dout_base_model)
 
-    self.RCNN_top = vgg.classifier
+        self.RCNN_top = vgg.classifier
 
-    # not using the last maxpool layer
-    self.RCNN_cls_score = nn.Linear(4096, self.n_classes)
+        # not using the last maxpool layer
+        self.RCNN_cls_score = nn.Linear(self.detection_dimension,
+                                        self.n_classes)
 
-    if self.class_agnostic:
-      self.RCNN_bbox_pred = nn.Linear(4096, 4)
-    else:
-      self.RCNN_bbox_pred = nn.Linear(4096, 4 * self.n_classes)      
+        if self.class_agnostic:
+            self.RCNN_bbox_pred = nn.Linear(self.detection_dimension, 4)
+        else:
+            self.RCNN_bbox_pred = nn.Linear(self.detection_dimension,
+                                            4 * self.n_classes)
 
-  def _head_to_tail(self, pool5):
-    
-    pool5_flat = pool5.view(pool5.size(0), -1)
-    fc7 = self.RCNN_top(pool5_flat)
+    def _head_to_tail(self, pool5):
 
-    return fc7
+        pool5_flat = pool5.view(pool5.size(0), -1)
+        fc7 = self.RCNN_top(pool5_flat)
 
+        return fc7
