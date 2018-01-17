57fe7cac5718af0681fc8e98646b5e0a285ba2e5
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-17 09:43:58 +0800

    reformat to fit PEP8

--- lib/model/utils/config.py
+++ lib/model/utils/config.py
@@ -33,7 +33,7 @@ __C.TRAIN.GAMMA = 0.1
 # Step size for reducing the learning rate, currently only support one step
 __C.TRAIN.STEPSIZE = [30000]
 
-# Iteration intervals for showing the loss during training, on command line interface
+# Iteration intervals for showing loss during training, on cmd interface
 __C.TRAIN.DISPLAY = 10
 
 # Whether to double the learning rate for bias
@@ -48,8 +48,8 @@ __C.TRAIN.BIAS_DECAY = False
 # Whether to add ground truth boxes to the pool when sampling regions
 __C.TRAIN.USE_GT = False
 
-# Whether to use aspect-ratio grouping of training images, introduced merely for saving
-# GPU memory
+# Whether to use aspect-ratio grouping of training images, introduced merely
+# for saving GPU memory
 __C.TRAIN.ASPECT_GROUPING = False
 
 # The number of snapshots kept, older ones are deleted to save space
@@ -143,7 +143,8 @@ __C.TRAIN.RPN_NMS_THRESH = 0.7
 __C.TRAIN.RPN_PRE_NMS_TOP_N = 12000
 # Number of top scoring boxes to keep after applying NMS to RPN proposals
 __C.TRAIN.RPN_POST_NMS_TOP_N = 2000
-# Proposal height and width both need to be greater than RPN_MIN_SIZE (at orig image scale)
+# Proposal height and width both need to be greater than RPN_MIN_SIZE (at
+# orig image scale)
 __C.TRAIN.RPN_MIN_SIZE = 8
 # Deprecated (outside weights)
 __C.TRAIN.RPN_BBOX_INSIDE_WEIGHTS = (1.0, 1.0, 1.0, 1.0)
@@ -152,7 +153,8 @@ __C.TRAIN.RPN_BBOX_INSIDE_WEIGHTS = (1.0, 1.0, 1.0, 1.0)
 # Set to -1.0 to use uniform example weighting
 __C.TRAIN.RPN_POSITIVE_WEIGHT = -1.0
 # Whether to use all ground truth bounding boxes for training,
-# For COCO, setting USE_ALL_GT to False will exclude boxes that are flagged as ''iscrowd''
+# For COCO, setting USE_ALL_GT to False will exclude boxes that are flagged
+# as ''iscrowd''
 __C.TRAIN.USE_ALL_GT = True
 
 # Whether to tune the batch normalization parameters during training
@@ -187,22 +189,24 @@ __C.TEST.HAS_RPN = False
 # Test using these proposals
 __C.TEST.PROPOSAL_METHOD = 'gt'
 
-## NMS threshold used on RPN proposals
+# NMS threshold used on RPN proposals
 __C.TEST.RPN_NMS_THRESH = 0.7
-## Number of top scoring boxes to keep before apply NMS to RPN proposals
+# Number of top scoring boxes to keep before apply NMS to RPN proposals
 __C.TEST.RPN_PRE_NMS_TOP_N = 6000
 
-## Number of top scoring boxes to keep after applying NMS to RPN proposals
+# Number of top scoring boxes to keep after applying NMS to RPN proposals
 __C.TEST.RPN_POST_NMS_TOP_N = 300
 
-# Proposal height and width both need to be greater than RPN_MIN_SIZE (at orig image scale)
+# Proposal height and width both need to be greater than RPN_MIN_SIZE (at orig
+# image scale)
 __C.TEST.RPN_MIN_SIZE = 16
 
 # Testing mode, default to be 'nms', 'top' is slower but better
 # See report for details
 __C.TEST.MODE = 'nms'
 
-# Only useful when TEST.MODE is 'top', specifies the number of top proposals to select
+# Only useful when TEST.MODE is 'top', specifies the number of top proposals
+# to select
 __C.TEST.RPN_TOP_N = 5000
 
 #
@@ -217,7 +221,8 @@ __C.RESNET = edict()
 # resized to a square of POOLING_SIZE
 __C.RESNET.MAX_POOL = False
 
-# Number of fixed blocks during training, by default the first of all 4 blocks is fixed
+# Number of fixed blocks during training, by default the first of all 4 blocks
+# is fixed
 # Range: 0 (none) to 3 (all)
 __C.RESNET.FIXED_BLOCKS = 1
 
@@ -230,7 +235,8 @@ __C.MOBILENET = edict()
 # Whether to regularize the depth-wise filters during training
 __C.MOBILENET.REGU_DEPTH = False
 
-# Number of fixed layers during training, by default the first of all 14 layers is fixed
+# Number of fixed layers during training, by default the first of all 14 layers
+# is fixed
 # Range: 0 (none) to 12 (all)
 __C.MOBILENET.FIXED_LAYERS = 5
 
@@ -289,10 +295,10 @@ __C.POOLING_SIZE = 7
 __C.MAX_NUM_GT_BOXES = 20
 
 # Anchor scales for RPN
-__C.ANCHOR_SCALES = [8,16,32]
+__C.ANCHOR_SCALES = [8, 16, 32]
 
 # Anchor ratios for RPN
-__C.ANCHOR_RATIOS = [0.5,1,2]
+__C.ANCHOR_RATIOS = [0.5, 1, 2]
 
 # Feature stride for RPN
 __C.FEAT_STRIDE = [16, ]
@@ -302,98 +308,102 @@ __C.CUDA = False
 __C.CROP_RESIZE_WITH_MAX_POOL = True
 
 import pdb
-def get_output_dir(imdb, weights_filename):
-  """Return the directory where experimental artifacts are placed.
-  If the directory does not exist, it is created.
 
-  A canonical path is built using the name from an imdb and a network
-  (if not None).
-  """
-  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'output', __C.EXP_DIR, imdb.name))
-  if weights_filename is None:
-    weights_filename = 'default'
-  outdir = osp.join(outdir, weights_filename)
-  if not os.path.exists(outdir):
-    os.makedirs(outdir)
-  return outdir
+
+def get_output_dir(imdb, weights_filename):
+    """Return the directory where experimental artifacts are placed.
+    If the directory does not exist, it is created.
+
+    A canonical path is built using the name from an imdb and a network
+    (if not None).
+    """
+    outdir = osp.abspath(
+        osp.join(__C.ROOT_DIR, 'output', __C.EXP_DIR, imdb.name))
+    if weights_filename is None:
+        weights_filename = 'default'
+    outdir = osp.join(outdir, weights_filename)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    return outdir
 
 
 def get_output_tb_dir(imdb, weights_filename):
-  """Return the directory where tensorflow summaries are placed.
-  If the directory does not exist, it is created.
+    """Return the directory where tensorflow summaries are placed.
+    If the directory does not exist, it is created.
 
-  A canonical path is built using the name from an imdb and a network
-  (if not None).
-  """
-  outdir = osp.abspath(osp.join(__C.ROOT_DIR, 'tensorboard', __C.EXP_DIR, imdb.name))
-  if weights_filename is None:
-    weights_filename = 'default'
-  outdir = osp.join(outdir, weights_filename)
-  if not os.path.exists(outdir):
-    os.makedirs(outdir)
-  return outdir
+    A canonical path is built using the name from an imdb and a network
+    (if not None).
+    """
+    outdir = osp.abspath(
+        osp.join(__C.ROOT_DIR, 'tensorboard', __C.EXP_DIR, imdb.name))
+    if weights_filename is None:
+        weights_filename = 'default'
+    outdir = osp.join(outdir, weights_filename)
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    return outdir
 
 
 def _merge_a_into_b(a, b):
-  """Merge config dictionary a into config dictionary b, clobbering the
-  options in b whenever they are also specified in a.
-  """
-  if type(a) is not edict:
-    return
+    """Merge config dictionary a into config dictionary b, clobbering the
+    options in b whenever they are also specified in a.
+    """
+    if type(a) is not edict:
+        return
 
-  for k, v in a.items():
-    # a must specify keys that are in b
-    if k not in b:
-      raise KeyError('{} is not a valid config key'.format(k))
+    for k, v in a.items():
+        # a must specify keys that are in b
+        if k not in b:
+            raise KeyError('{} is not a valid config key'.format(k))
 
-    # the types must match, too
-    old_type = type(b[k])
-    if old_type is not type(v):
-      if isinstance(b[k], np.ndarray):
-        v = np.array(v, dtype=b[k].dtype)
-      else:
-        raise ValueError(('Type mismatch ({} vs. {}) '
-                          'for config key: {}').format(type(b[k]),
-                                                       type(v), k))
+        # the types must match, too
+        old_type = type(b[k])
+        if old_type is not type(v):
+            if isinstance(b[k], np.ndarray):
+                v = np.array(v, dtype=b[k].dtype)
+            else:
+                raise ValueError(('Type mismatch ({} vs. {}) '
+                                  'for config key: {}').format(type(b[k]),
+                                                               type(v), k))
 
-    # recursively merge dicts
-    if type(v) is edict:
-      try:
-        _merge_a_into_b(a[k], b[k])
-      except:
-        print(('Error under config key: {}'.format(k)))
-        raise
-    else:
-      b[k] = v
+        # recursively merge dicts
+        if type(v) is edict:
+            try:
+                _merge_a_into_b(a[k], b[k])
+            except:
+                print(('Error under config key: {}'.format(k)))
+                raise
+        else:
+            b[k] = v
 
 
 def cfg_from_file(filename):
-  """Load a config file and merge it into the default options."""
-  import yaml
-  with open(filename, 'r') as f:
-    yaml_cfg = edict(yaml.load(f))
+    """Load a config file and merge it into the default options."""
+    import yaml
+    with open(filename, 'r') as f:
+        yaml_cfg = edict(yaml.load(f))
 
-  _merge_a_into_b(yaml_cfg, __C)
+    _merge_a_into_b(yaml_cfg, __C)
 
 
 def cfg_from_list(cfg_list):
-  """Set config keys via list (e.g., from command line)."""
-  from ast import literal_eval
-  assert len(cfg_list) % 2 == 0
-  for k, v in zip(cfg_list[0::2], cfg_list[1::2]):
-    key_list = k.split('.')
-    d = __C
-    for subkey in key_list[:-1]:
-      assert subkey in d
-      d = d[subkey]
-    subkey = key_list[-1]
-    assert subkey in d
-    try:
-      value = literal_eval(v)
-    except:
-      # handle the case when v is a string literal
-      value = v
-    assert type(value) == type(d[subkey]), \
-      'type {} does not match original type {}'.format(
-        type(value), type(d[subkey]))
-    d[subkey] = value
+    """Set config keys via list (e.g., from command line)."""
+    from ast import literal_eval
+    assert len(cfg_list) % 2 == 0
+    for k, v in zip(cfg_list[0::2], cfg_list[1::2]):
+        key_list = k.split('.')
+        d = __C
+        for subkey in key_list[:-1]:
+            assert subkey in d
+            d = d[subkey]
+        subkey = key_list[-1]
+        assert subkey in d
+        try:
+            value = literal_eval(v)
+        except:
+            # handle the case when v is a string literal
+            value = v
+        assert type(value) == type(d[subkey]), \
+            'type {} does not match original type {}'.format(
+                type(value), type(d[subkey]))
+        d[subkey] = value
