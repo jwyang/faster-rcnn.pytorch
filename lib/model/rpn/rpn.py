a17e5efc773ba709f87b182ae4129ff60f7fa7db
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-18 15:58:31 +0800

    make rpn channel able to be chosen

--- lib/model/rpn/rpn.py
+++ lib/model/rpn/rpn.py
@@ -24,20 +24,23 @@ class _RPN(nn.Module):
         self.anchor_scales = cfg.ANCHOR_SCALES
         self.anchor_ratios = cfg.ANCHOR_RATIOS
         self.feat_stride = cfg.FEAT_STRIDE[0]
+        self.rpn_channel = 512  # set RPN channels rather than fixed 512
 
         # define the convrelu layers processing input feature map with padding
-        # TODO: set RPN channels rather than fixed 512
-        self.RPN_Conv = nn.Conv2d(self.din, 512, 3, 1, 1, bias=True)
 
+        self.RPN_Conv = nn.Conv2d(
+            self.din, self.rpn_channel, 3, 1, 1, bias=True)
         # define bg/fg classifcation score layer
         self.nc_score_out = len(self.anchor_scales) * len(
             self.anchor_ratios) * 2  # 2(bg/fg) * 9 (anchors)
-        self.RPN_cls_score = nn.Conv2d(512, self.nc_score_out, 1, 1, 0)
+        self.RPN_cls_score = nn.Conv2d(
+            self.rpn_channel, self.nc_score_out, 1, 1, 0)
 
         # define anchor box offset prediction layer
         self.nc_bbox_out = len(self.anchor_scales) * len(
             self.anchor_ratios) * 4  # 4(coords) * 9 (anchors)
-        self.RPN_bbox_pred = nn.Conv2d(512, self.nc_bbox_out, 1, 1, 0)
+        self.RPN_bbox_pred = nn.Conv2d(
+            self.rpn_channel, self.nc_bbox_out, 1, 1, 0)
 
         # define proposal layer
         self.RPN_proposal = _ProposalLayer(self.feat_stride,
