1515da5d101e1e64ddb3295e95e3329f60020939
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-21 11:23:49 +0800

    change the dataset

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ GPU_ID=$1
 NET=$2
 EPOCHS=$3
 
-DATASET="pascal_voc"
+DATASET="psdb"
 
 array=( $@ )
 len=${#array[@]}
@@ -19,7 +19,7 @@ exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 CUDA_VISIBLE_DEVICES=${GPU_ID} python trainval_net.py \
-    --dataset pascal_voc \
+    --dataset ${DATASET} \
     --net ${NET} \
     --epochs ${EPOCHS} \
     --cuda \
