7e9e14921f1bdba30c37a9accd86ecc600c50468
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-26 10:26:20 +0800

    add res101 pretrained model

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 cp ../vgg16_caffe.pth ./data/pretrained_model/
+cp ../resnet101_caffe.pth ./data/pretrained_model/
 
 cd lib
 sh make.sh
 cd ../data
 
 ln -s ../../VOCdevkit VOCdevkit2007
+ln -s ../../person_search/data/psdb psdb
 cd ..
