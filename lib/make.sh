f9e030c07611982731d7730adbc235ed50a92e97
Liangqi Li <Lewis_lee@sjtu.edu.cn>
2018-01-27 16:28:41 +0800

    change computional setting

--- lib/make.sh
+++ lib/make.sh
@@ -9,7 +9,7 @@ rm -rf build
 cd model/nms/src
 echo "Compiling nms kernels by nvcc..."
 nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_61
 
 cd ../
 python build.py
@@ -19,7 +19,7 @@ cd ../../
 cd model/roi_pooling/src
 echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_61
 cd ../
 python build.py
 
@@ -28,7 +28,7 @@ cd ../../
 cd model/roi_align/src
 echo "Compiling roi align kernels by nvcc..."
 nvcc -c -o roi_align_kernel.cu.o roi_align_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_61
 cd ../
 python build.py
 
@@ -37,6 +37,6 @@ cd ../../
 cd model/roi_crop/src
 echo "Compiling roi crop kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu \
-	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_52
+	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_61
 cd ../
 python build.py
