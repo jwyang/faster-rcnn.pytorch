70d7381dc98278498a8f07374ea72eef6e297a8e
landanjs <landanjs504@gmail.com>
2018-03-05 00:03:53 -0500

    add webcam realtime demo

--- demo.py
+++ demo.py
@@ -92,6 +92,10 @@ def parse_args():
   parser.add_argument('--vis', dest='vis',
                       help='visualization mode',
                       action='store_true')
+  parser.add_argument('--webcam_num', dest='webcam_num',
+                      help='webcam ID number',
+                      default=-1, type=int)
+
   args = parser.parse_args()
   return args
 
@@ -225,18 +229,34 @@ if __name__ == '__main__':
   thresh = 0.05
   vis = True
 
-  imglist = os.listdir(args.image_dir)
-  num_images = len(imglist)
+  webcam_num = args.webcam_num
+  # Set up webcam or get image directories
+  if webcam_num >= 0 :
+    cap = cv2.VideoCapture(webcam_num)
+    num_images = 0
+  else:
+    imglist = os.listdir(args.image_dir)
+    num_images = len(imglist)
 
   print('Loaded Photo: {} images.'.format(num_images))
 
 
-  for i in range(num_images):
+  while (num_images >= 0):
+      total_tic = time.time()
+      if webcam_num == -1:
+        num_images -= 1
 
+      # Get image from the webcam
+      if webcam_num >= 0:
+        if not cap.isOpened():
+          raise RuntimeError("Webcam could not open. Please check connection.")
+        ret, frame = cap.read()
+        im_in = np.array(frame)
       # Load the demo image
-      im_file = os.path.join(args.image_dir, imglist[i])
-      # im = cv2.imread(im_file)
-      im_in = np.array(imread(im_file))
+      else:
+        im_file = os.path.join(args.image_dir, imglist[num_images])
+        # im = cv2.imread(im_file)
+        im_in = np.array(imread(im_file))
       if len(im_in.shape) == 2:
         im_in = im_in[:,:,np.newaxis]
         im_in = np.concatenate((im_in,im_in,im_in), axis=2)
@@ -319,12 +339,25 @@ if __name__ == '__main__':
       misc_toc = time.time()
       nms_time = misc_toc - misc_tic
 
-      sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
-          .format(i + 1, num_images, detect_time, nms_time))
-      sys.stdout.flush()
+      if webcam_num == -1:
+          sys.stdout.write('im_detect: {:d}/{:d} {:.3f}s {:.3f}s   \r' \
+                           .format(num_images + 1, len(imglist), detect_time, nms_time))
+          sys.stdout.flush()
 
-      if vis:
+      if vis and webcam_num == -1:
           # cv2.imshow('test', im2show)
           # cv2.waitKey(0)
-          result_path = os.path.join(args.image_dir, imglist[i][:-4] + "_det.jpg")
+          result_path = os.path.join(args.image_dir, imglist[num_images][:-4] + "_det.jpg")
           cv2.imwrite(result_path, im2show)
+      else:
+          im2showRGB = cv2.cvtColor(im2show, cv2.COLOR_BGR2RGB)
+          cv2.imshow("frame", im2showRGB)
+          total_toc = time.time()
+          total_time = total_toc - total_tic
+          frame_rate = 1 / total_time
+          print('Frame rate:', frame_rate)
+          if cv2.waitKey(1) & 0xFF == ord('q'):
+              break
+  if webcam_num >= 0:
+      cap.release()
+      cv2.destroyAllWindows()
