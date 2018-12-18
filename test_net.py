7e10afa18f8a088e5eb31e3031b7c2bf9f468c69
Aditya Arun <adityaarun1@gmail.com>
2018-12-18 19:47:57 +0530

    Update test_net.py for Pytorch 1.0

--- test_net.py
+++ test_net.py
@@ -25,7 +25,8 @@ from roi_data_layer.roidb import combined_roidb
 from roi_data_layer.roibatchLoader import roibatchLoader
 from model.utils.config import cfg, cfg_from_file, cfg_from_list, get_output_dir
 from model.rpn.bbox_transform import clip_boxes
-from model.nms.nms_wrapper import nms
+# from model.nms.nms_wrapper import nms
+from model.roi_layers import nms
 from model.rpn.bbox_transform import bbox_transform_inv
 from model.utils.net_utils import save_net, load_net, vis_detections
 from model.faster_rcnn.vgg16 import vgg16
