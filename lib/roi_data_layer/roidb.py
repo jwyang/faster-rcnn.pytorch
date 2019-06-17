a2706b89c9b7ba9e64bce0fb2688c876deb9cf8e
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-06-17 14:20:13 +0800

    Remove "for training" in get_roidb
    get_roidb will be indirectly called by test_net.py.
    This PR removes "for training" because it's misleading.

--- lib/roi_data_layer/roidb.py
+++ lib/roi_data_layer/roidb.py
@@ -120,7 +120,7 @@ def combined_roidb(imdb_names, training=True):
   
   def get_roidb(imdb_name):
     imdb = get_imdb(imdb_name)
-    print('Loaded dataset `{:s}` for training'.format(imdb.name))
+    print('Loaded dataset `{:s}`'.format(imdb.name))
     imdb.set_proposal_method(cfg.TRAIN.PROPOSAL_METHOD)
     print('Set proposal method: {:s}'.format(cfg.TRAIN.PROPOSAL_METHOD))
     roidb = get_training_roidb(imdb)
