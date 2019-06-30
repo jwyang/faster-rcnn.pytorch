ff37838d40b480c067ddcc8df22445677a960197
vaslem <vasilis.lemonidis@metis.tech>
2019-07-01 02:22:48 +0300

    Moved files to subfolder and exposed scripts to console-scripts setuptools mechanism. Now the user can call them by issuing the command faster_rcnn_NAME_OF_THE_SCRIPT in terminal or cmd

---
+++ faster_rcnn/lib/model/nms/nms_wrapper.py
@@ -0,0 +1,21 @@
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+import torch
+from faster_rcnn.lib.model.utils.config import cfg
+if torch.cuda.is_available():
+    from faster_rcnn.lib.model.nms.nms_gpu import nms_gpu
+from faster_rcnn.lib.model.nms.nms_cpu import nms_cpu
+
+def nms(dets, thresh, force_cpu=False):
+    """Dispatch to either CPU or GPU NMS implementations."""
+    if dets.shape[0] == 0:
+        return []
+    # ---numpy version---
+    # original: return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
+    # ---pytorch version---
+
+    return nms_gpu(dets, thresh) if force_cpu == False else nms_cpu(dets, thresh)
