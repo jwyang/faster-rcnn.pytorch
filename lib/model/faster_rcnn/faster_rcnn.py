5123bcb54f9a31b8c270f45935d57d9d894572c3
keineahnung2345 <mimifasosofamire1123@gmail.com>
2019-07-15 18:29:24 +0800

    Fix typo in faster_rcnn.py

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -46,7 +46,7 @@ class _fasterRCNN(nn.Module):
         # feed image data to base model to obtain base feature map
         base_feat = self.RCNN_base(im_data)
 
-        # feed base feature map tp RPN to obtain rois
+        # feed base feature map to RPN to obtain rois
         rois, rpn_loss_cls, rpn_loss_bbox = self.RCNN_rpn(base_feat, im_info, gt_boxes, num_boxes)
 
         # if it is training phase, then use ground truth bboxes for refining
