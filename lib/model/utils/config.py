30f11ec3d5fbd9eeff755f04e74c904c08d6e77b
Cagdas Bas <cagdasbs@gmail.com>
2022-05-19 23:33:52 +0300

    fix: update everything for new dependencies

--- lib/model/utils/config.py
+++ lib/model/utils/config.py
@@ -371,7 +371,7 @@ def cfg_from_file(filename):
   """Load a config file and merge it into the default options."""
   import yaml
   with open(filename, 'r') as f:
-    yaml_cfg = edict(yaml.load(f))
+    yaml_cfg = edict(yaml.safe_load(f))
 
   _merge_a_into_b(yaml_cfg, __C)
 
